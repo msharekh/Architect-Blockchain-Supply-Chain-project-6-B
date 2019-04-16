bedb5bce42ceb5a487f9721075f5b3cce5259ec4
Mishal Alsharekh <mishal.sharekh@gmail.com>
2019-04-16 23:23:00 +0300

    before unistall truffle

--- truffle.js
+++ truffle.js
@@ -1,40 +1,9 @@
-// var HDWalletProvider = require('truffle-hdwallet-provider');
+var HDWalletProvider = require('truffle-hdwallet-provider');
 // const infuraKey = '<Infura PROJECT ID>';
-// var mnemonic =
-//   'spirit supply whale amount human item harsh scare congress discover talent hamster';
+var mnemonic =
+  'uphold picnic age aunt sheriff embody dune gauge merit hero purse innocent';
 
-// module.exports = {
-//   networks: {
-//     development: {
-//       host: '127.0.0.1',
-//       port: 8545,
-//       network_id: '*' // Match any network id
-//     },
-//     rinkeby: {
-//       provider: function() {
-//         return new HDWalletProvider(
-//           mnemonic,
-//           'mainnet.infura.io/v3/2dcdc64520ac497481d2fd701b1c9641'
-//         );
-//       },
-//       from: '0x66178bd1f010b4bF530203f90f889b599874fF84',
-//       network_id: 4,
-//       gas: 4500000,
-//       gasPrice: 10000000000
-//     }
-//   }
-// };
-
-const HDWalletProvider = require('truffle-hdwallet-provider');
 module.exports = {
-  compilers: {
-    solc: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  },
   networks: {
     development: {
       host: '127.0.0.1',
@@ -42,16 +11,47 @@ module.exports = {
       network_id: '*' // Match any network id
     },
     rinkeby: {
-      provider: () => {
-        //replace string parameters
+      provider: function() {
         return new HDWalletProvider(
-          'spirit supply whale amount human item harsh scare congress discover talent hamster',
-          'mainnet.infura.io/v3/2dcdc64520ac497481d2fd701b1c9641'
+          mnemonic,
+          'rinkeby.infura.io/v3/40e4f2f4d71b4ca2971de016062f5846'
         );
       },
+      // from: '0x66178bd1f010b4bF530203f90f889b599874fF84',
       network_id: 4,
-      gas: 6700000,
+      gas: 4500000,
       gasPrice: 10000000000
     }
   }
 };
+
+// const HDWalletProvider = require('truffle-hdwallet-provider');
+// module.exports = {
+//   compilers: {
+//     solc: {
+//       optimizer: {
+//         enabled: true,
+//         runs: 200
+//       }
+//     }
+//   },
+//   networks: {
+//     development: {
+//       host: '127.0.0.1',
+//       port: 8545,
+//       network_id: '*' // Match any network id
+//     },
+//     rinkeby: {
+//       provider: () => {
+//         //replace string parameters
+//         return new HDWalletProvider(
+//           'vacuum stuff swap chaos bulb design suit mask ticket october note private',
+//           'rinkeby.infura.io/v3/5f756d9eb86749e1ad58bd0a563e4b9b'
+//         );
+//       },
+//       network_id: 4,
+//       gas: 6700000,
+//       gasPrice: 10000000000
+//     }
+//   }
+// };
